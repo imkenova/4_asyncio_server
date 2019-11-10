1978b7f7e6e5e60806626beaf6cab4b051b065c1
Елена Имкенова <lena_imkenova@MacBook-Air-Elena.local>
2019-11-10 16:23:57 +0300

    Чат

--- client.py
+++ client.py
@@ -1,24 +1,83 @@
-import asyncio
+import socket
+import threading
+
+def update_chat():
+    global username
+    while True:
+        answer = sock.recv(1024)	
+        if '@'+username in answer.decode().split():	
+            print("Вас упомянули в сообщении")
+            print(f'\n',answer.decode())
+        else:	
+            print(f'\n',answer.decode())
 
 
-HOST = 'localhost'
-PORT = 9090
+host = input('Введите имя хоста: ')
+if host == 'localhost':
+    pass
+else:
+    if any(c.isalpha() for c in host) == True:
+        print('Введено некорректное имя хоста.По умолчанию выбран локальный хост')
+        host = 'localhost'
+    else:
+        host_lst = host.split('.')           
+        for i in host_lst:
+            if 0 <= int(i) <= 255:
+                pass
+            else:
+                host = 'localhost'
+                print('Введено некорректное имя хоста.По умолчанию выбран локальный хост')
+
+try:
+    port = int(input('Введите номер порта: '))
+    if 0 <= port <= 65535:
+        pass
+    else:
+        print('Введен некорректный номер порта.Номер порта по умолчанию 9090')
+        port = 9090
+        
+except ValueError:
+    print("Некорректный номер порта. Номер порта по умолчанию 9090")
+    port = 9090  
+
+sock = socket.socket()
+sock.connect((host,port))
+
+username = ''
+password = ''
+
+while not username:     
+    username = input('Введите Ваше имя: ')
+
+sock.send(username.encode())		
+answer = sock.recv(1024)
 
 
-async def tcp_echo_client(host, port):
-    reader, writer = await asyncio.open_connection(host, port)
-    message = input("Enter message: ")
+if answer.decode() == 'registration':
+    print('Вы еще не зарегистрированы.')
+    print(f'Регистрируемое имя: {username}')
+    while not password:	
+        password = input('Придумайте пароль:')
+    sock.send(password.encode())	
+    answer = sock.recv(1024)
+    if answer.decode() == 'completed':	
+        print('Регистрация завершена. Пожалуйста,перезапустите программу')
+else:
+    password = input('Введите пароль: ')
+    sock.send(password.encode())
+    answer = sock.recv(1024)
+    if answer.decode() == 'valid':     
+        print("Для упоминания пользователя используйте формат @имя")
+        print("Для выхода из чата введите exit")
+        threading.Thread(target=update_chat,args=()).start()
+        while True:	
+            msg = input('')
+            if msg == 'exit':
+                sock.send('exit'.encode())
+                break
+            sock.send(msg.encode())
+    else:
+        print('Неправильный пароль')
 
-    writer.write(message.encode())
-    await writer.drain()
-
-    data = await reader.read(100)
-    print(data.decode())
-    writer.close()
-    await writer.wait_closed()
-
-
-
-loop = asyncio.get_event_loop()
-task = loop.create_task(tcp_echo_client(HOST, PORT))
-loop.run_until_complete(task)
+sock.close()
+print("Работа с сервером завершена.")		
--- server.py
+++ server.py
@@ -1,33 +1,70 @@
 import asyncio
+import os
+import pickle
+
+users_file = 'clients.pickle'      
+users = {}
+connected_users = []
 
 HOST = 'localhost'
 PORT = 9090
 
 
 async def handle_echo(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
+    global connected_users		
+    username = await reader.read(100)
+    if username.decode() in users.keys():      
+        password = users[username.decode()]
+        writer.write('ask_password'.encode())			
+        userpassword = await reader.read(1024)	
+        if userpassword.decode() == password:		
+            writer.write('valid'.encode())		 
+            print(f'{username.decode()} присоединился к чату')		
+            connected_users.append(writer)		
+            while True:		
+                msg = await reader.read(1024)	
+                if msg.decode() == 'exit':		
+                    print(f'{username.decode()} покинул чат')		
+                    leftmsg = f'{username.decode()} покинул чат.'		
+                    for user in connected_users:	
+                        if user != writer:	
+                            user.write(leftmsg.encode())
+                    connected_users.remove(writer)
+                    break
+                msg = f'[{username.decode()}]: ' + msg.decode() + '\n '
+                for user in connected_users:
+                    if user != writer:
+                        user.write(msg.encode())
+                        
+        else:
+            writer.write('invalid'.encode())		
+            writer.close()
+    else:
+        writer.write('registration'.encode())		
+        password = await reader.read(1024)
+        users.update({username.decode():password.decode()})
+        with open(users_file,'wb') as f:
+            pickle.dump(users,f)
+        writer.write('completed'.encode())
+        writer.close()
 
-    print(data.decode())
-    writer.write(data)
-    await writer.drain()
-
-    writer.close()
+if os.path.exists(users_file):        
+    with open(users_file,'rb') as f:
+        users = pickle.load(f)
+else:
+    users = {}
 
 
 loop = asyncio.get_event_loop()
 coro = asyncio.start_server(handle_echo, HOST, PORT, loop=loop)
 server = loop.run_until_complete(coro)
 
-
-print('Serving on {}'.format(server.sockets[0].getsockname()))
+print('Serving on {}'.format(server.sockets[0].getsockname()))		
 try:
     loop.run_forever()
 except KeyboardInterrupt:
     pass
 
-
 server.close()
 loop.run_until_complete(server.wait_closed())
-
 loop.close()
